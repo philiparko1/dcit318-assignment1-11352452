58d517febacbf662b9a3e63ca186093ac28e480a
philiparko <layricky51@gmail.com>
2025-07-31 00:36:51 +0000

    Implemented Triangle Type Identifier: Classifies triangles by sides

---
+++ TriangleTypeIdentifier/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter three sides of a triangle:");
+        double a = double.Parse(Console.ReadLine());
+        double b = double.Parse(Console.ReadLine());
+        double c = double.Parse(Console.ReadLine());
+
+        string type = (a == b && b == c) ? "Equilateral" :
+                      (a == b || a == c || b == c) ? "Isosceles" : "Scalene";
+
+        Console.WriteLine($"Triangle type: {type}");
+    }
+}
